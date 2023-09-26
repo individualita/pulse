df70a471bd1b69d48b72650552d77a2ce5e34d9b
Roman <asdwka3@gmail.com>
2023-09-26 23:06:47 +0200

    added new section with tabs

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         speed: 1100,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
