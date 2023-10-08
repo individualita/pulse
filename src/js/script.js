a9bb5dec50825dc8c96a4d061ef419fa0252bd54
Roman <asdwka3@gmail.com>
2023-10-08 20:12:01 +0200

    added smooth pageup scroll

--- src/js/script.js
+++ src/js/script.js
@@ -102,4 +102,19 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //scroll and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop () > 700) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut('slow');
+        }
+    });
+    //smooth scroll
+    $("a[href^='#']").click (function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    })
  });
