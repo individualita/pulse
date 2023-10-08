c70764e17637f54ddfc3c901e5a162610e23c24c
Roman <asdwka3@gmail.com>
2023-10-08 17:59:55 +0200

    added phpmailer

--- src/js/script.js
+++ src/js/script.js
@@ -80,4 +80,26 @@ $(document).ready(function(){
     valideForms('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    //ajax
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if(!(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
  });
