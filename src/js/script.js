3e7dc53454ab329fa630c62d1e21221dec18ef02
Roman <asdwka3@gmail.com>
2023-10-10 22:46:09 +0200

    added wow animations

--- src/js/script.js
+++ src/js/script.js
@@ -116,5 +116,6 @@ $(document).ready(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
-    })
+    });
+    new WOW().init();
  });
